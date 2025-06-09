e698d67172a1507999c4daae18f86ea196e21374
harry <contact@harry.plus>
2025-06-09 14:52:14 +0530

    wip

--- src/Middleware/EnsureIpnIsValid.php
+++ src/Middleware/EnsureIpnIsValid.php
@@ -4,26 +4,28 @@ declare(strict_types=1);
 
 namespace Harryqt\NowPayments\Middleware;
 
+use Closure;
+use RuntimeException;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class EnsureIpnIsValid
 {
-    public function handle(Request $request, \Closure $next): Response
+    public function handle(Request $request, Closure $next): Response
     {
         if (! $request->hasHeader('x-nowpayments-sig')) {
-            throw new \RuntimeException('x-nowpayments-sig header is missing.');
+            throw new RuntimeException('x-nowpayments-sig header is missing.');
         }
 
         if (! $requestData = $request->all()) {
-            throw new \RuntimeException('input fields are missing.');
+            throw new RuntimeException('input fields are missing.');
         }
 
         ksort($requestData);
         $hash = hash_hmac('sha512', json_encode($requestData), config('nowpayments.ipn_secret'));
 
         if ($hash !== $request->header('x-nowpayments-sig')) {
-            throw new \RuntimeException('Calculated hash differs from received hash.');
+            throw new RuntimeException('Calculated hash differs from received hash.');
         }
 
         return $next($request);
